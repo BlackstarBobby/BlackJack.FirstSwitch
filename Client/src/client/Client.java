25238124dbe2a427df18352d5e14d35cb6bfcb5f
Cretu Calin <cretucalin06@yahoo.ro>
2016-07-16 11:23:19 +0300

    Commit 1.0
    Chance project with the other team

---
+++ Client/src/client/Client.java
@@ -0,0 +1,127 @@
+package client;
+
+/**
+ * Created by Cretu Calin on 7/16/2016.
+ */
+
+        import java.io.IOException;
+        import java.io.ObjectInputStream;
+        import java.io.ObjectOutputStream;
+        import java.net.Socket;
+        import java.net.UnknownHostException;
+        import java.util.Scanner;
+
+
+public class Client {
+
+    private String IP;
+    private int port;
+    private Socket socket;
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+    private String message;
+    private Scanner scanner;
+
+    public Client(String IP, int port){
+
+        this.IP = IP;
+        this.port = port;
+        scanner = new Scanner(System.in);
+
+    }
+
+    public void connectToServer() {
+        System.out.println("Attempting connection...\n");
+        try {
+            socket = new Socket(IP, port);
+            System.out.println("Connected to server\n");
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void setUpStreams() {
+        try {
+            output = new ObjectOutputStream(socket.getOutputStream());
+            output.flush();
+            input = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Streams are up\n");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String message)
+    {
+        try {
+            output.writeObject(message);
+            output.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void close() {
+        try
+        {   scanner.close();
+            output.close();
+            input.close();
+            socket.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+
+        connectToServer();
+        setUpStreams();
+        // TODO Auto-generated method stub
+        while( true ){
+
+            try {
+                message = (String) input.readObject();
+
+
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println(message);
+                break;
+            }
+            if (message.equals("It is your turn! HIT or STAND?")){
+                String line = "";
+                line = scanner.nextLine();
+                sendMessage(line);
+            }
+            close();
+        }
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Client/src/client/Main.java
@@ -0,0 +1,16 @@
+package client;
+
+/**
+ * Created by Cretu Calinn on 7/16/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Client client = new Client ("localhost",9999);
+        client.start();
+
+    }
+
+}
+
