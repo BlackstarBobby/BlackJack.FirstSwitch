b729d98295b27668a10716cc688bf951617b0f8b
Rares Cristea <raresito@gmail.com>
2016-07-16 22:04:54 +0300

    Revert "Card + deck modifications"
    This reverts commit 2cfe5b6a7e9d6261970bf1b65a4442374b0da758.

--- MainServer/src/server/Deck.java
+++ MainServer/src/server/Deck.java
@@ -1,31 +1,62 @@
-package deck;
+package server;
+
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Created by Cata on 7/6/2016.
+ */
 
 public class Deck {
-    ArrayList<Card> pachet;
+    public enum Suit
+    {
+        DIAMONDS, HEARTS, SPADES, CLUBS
+    }
+    private ArrayList<Card> cards;
 
-    public Deck(){
-        pachet = new ArrayList<Card>();
-        for(int i=1;i<=13;i++)
-        {
-            pachet.add(new Card(i,"diamonds"));
-            pachet.add(new Card(i,"clubs"));
-            pachet.add(new Card(i,"hearts"));
-            pachet.add(new Card(i,"spades"));
+
+
+    public Deck()
+    {
+        cards=new ArrayList<Card>();
+        int j=0;
+        for (int i=2;i<=14;i++) {
+            for(Suit s : Suit.values())
+            {
+                cards.add(new Card(s.toString(),i));
+            }
         }
         shuffle();
     }
-
+    public String toString(){
+        return String.format("in pachet sunt %s de carti", cards.size());
+    }
     public void shuffle(){
-        Collections.shuffle(pachet);
+        Collections.shuffle(cards);
+    }
+    public Card drawCard()
+    {
+        Card tmp=cards.get(0);
+        cards.remove(0);
+        return tmp;
     }
 
-    public Card getCard() {
+
+
+    //not needed for arraylists
+    private void shuffleArray(Card[] array)
+    {
+        int index;
         Card temp;
-        temp = pachet.get(pachet.size()-1);
-        pachet.remove(pachet.size()-1);
-        return temp;
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--)
+        {
+            index = random.nextInt(i + 1);
+            temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
     }
 }
